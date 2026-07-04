4b3830e046d57a74462d6109d442a454ab24dd1a
hasanov.h <metaitschool448@gmail.com>
2026-07-04 17:13:31 +0500

    ulandi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Firdavs from './Firdavs/Firdavs'
 import Mrizo from './Mrizo/Mrizo'
+import Asilbek from './Asilbek/Asilbek'
 
 const App = () => {
   return (
     <div>
-      <Firdavs />
+      <Asilbek />
     </div>
   )
 }

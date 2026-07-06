958718878efe48f4afe65edeadb56c76b83041fb
Firdavs0409 <firdavsnumonov2011@gmail.com>
2026-07-06 18:42:09 +0500

    sdfhjkl;

--- src/Asilbek/Asilbek.jsx
+++ src/Asilbek/Asilbek.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Asilbek.css'
 import logo from './logo.png'
+import img2 from './r1.jpg'
+import img1 from './img.jpg'
 import { BrowserRouter, Route, Router } from 'react-router-dom'
 import Firdavs from '../Firdavs/Firdavs'
 import Mrizo from '../Mrizo/Mrizo'
@@ -22,12 +24,24 @@ const Asilbek = () => {
                 </ul>
             </div>
             <div className="orta">
-                <div className="orta_t">
-                    <p>Home</p>
-                    <p>|</p>
-                    <p>Menu</p>
+                <div className="orta_p">
+                    <div className="pass">
+                        <div className="chap">
+                            <p className='ppp'>instagramm</p>
+                            <h4 className='h4'>Photo Gallery</h4>
+                            <p className='pp1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+                            <button className='bt1'>View More</button>
+                        </div>
+                        <div className="ong">
+                            <img src={img2} alt="" />
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4RVhwtDgJF_-reA0-7oNg7lBZkQxxIei3WzbKHyhKyw&s" alt="" />
+                            <img src={img1} alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
+
+            
         </div>
 
     </div>

2d09fab7a68df703c57cb55aa5adf0f063d332de
Kamrobek1774-Kali <aligangbot@gmail.com>
2026-07-08 16:01:47 +0500

    backend qo'shildi

---
+++ backend/backend/frontend/models/Navbar.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "navbar".
+ *
+ * @property int $id
+ * @property string $nom
+ * @property string $link
+ */
+class Navbar extends \yii\db\ActiveRecord
+{
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'navbar';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nom', 'link'], 'required'],
+            [['nom', 'link'], 'string', 'max' => 60],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nom' => 'Nom',
+            'link' => 'Link',
+        ];
+    }
+
+}
